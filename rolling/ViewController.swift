3645a059a9d75c30db13002b6ed09a9042af99ce
kellychang <jaytoztoz@gmail.com>
2017-12-04 11:57:45 +0800

    first commit

--- rolling/ViewController.swift
+++ rolling/ViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var randomNumber1 : Int = 0
+    var randomNumber2 : Int = 0
+    let imageArray = ["1","2","3","4","5","6"]
+    
+    
+    @IBOutlet weak var diceImage1: UIImageView!
+    
+    @IBOutlet weak var diceimage2: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       updateDiceImage()
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func rollButton(_ sender: UIButton) {
+        
+      updateDiceImage()
+        
+        
+    }
+    func updateDiceImage(){
+        randomNumber1 = Int(arc4random_uniform(6))
+        randomNumber2 = Int(arc4random_uniform(6))
+        diceImage1.image = UIImage(named:imageArray[randomNumber1])
+        diceimage2.image = UIImage(named:imageArray[randomNumber2])
 
 }
-
+}
